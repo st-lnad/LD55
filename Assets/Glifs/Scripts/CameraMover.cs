e4d920479cf39dba6e88eb137d7e65c699999d13
st-lnad <lev.potanin@yandex.ru>
2024-04-15 18:12:17 +0300

    save updates

--- Assets/Glifs/Scripts/CameraMover.cs
+++ Assets/Glifs/Scripts/CameraMover.cs
@@ -6,16 +6,33 @@ public class CameraMover : MonoBehaviour
 {
     [SerializeField] private AnimationCurve _speed;
     [SerializeField, Min(0.01f)] private float _transitionTime;
+    [SerializeField] float _normalSize = 5.4f;
+    [SerializeField] float _scopeSize = 2f;
+    [SerializeField] private Glif _targetGlif;
 
+    private Camera _camera;
     private Vector3 previousPosition;
     private Vector3 glifPosition;
-    public void MoveToGlif(Glif glif)
+
+    private void Awake()
     {
+        _camera = GetComponent<Camera>();
+    }
+
+    [ContextMenu("Zoom")]
+    public void MoveToGlif()
+    {
+        Glif glif = _targetGlif;
+        _camera.enabled = true;
         previousPosition = transform.position;
         glifPosition = glif.transform.position;
         glifPosition.z = previousPosition.z;
         StartCoroutine(nameof(Move));
-        
+    }
+
+    public void ResetPosition()
+    {
+
     }
 
     private IEnumerator Move()
@@ -25,6 +42,7 @@ public class CameraMover : MonoBehaviour
         while (t <= 1f)
         {
             t += Time.deltaTime;
+            _camera.orthographicSize = Mathf.Lerp(_normalSize, _scopeSize, t);
             transform.position = Vector3.Lerp(previousPosition, glifPosition, t);
             yield return delay;
         }
